ec95a15cc1cfdd76a360073db7fe2518602d0bb9
Debrini Sarkar <dsarka4659@rcs-k12.us>
2018-02-03 10:37:54 -0500

    Added code for intake and outtake with flywheels.

--- src/com/github/adambots/powerup2018/flywheels/Flywheels.java
+++ src/com/github/adambots/powerup2018/flywheels/Flywheels.java
@@ -1,5 +1,23 @@
 package com.github.adambots.powerup2018.flywheels;
 
-public class Flywheels {
+import org.usfirst.frc.team245.robot.Actuators;
+import org.usfirst.frc.team245.robot.Constants;
+import org.usfirst.frc.team245.robot.Gamepad;
 
+public class Flywheels {
+	
+	public static void main (String args[]) {
+		
+		// TODO: check if it is max and min for each wheel during intake and outtake
+		// intake
+		if (Gamepad.secondary.getRT()) {
+			Actuators.getLeftArmWheel().set(Constants.MAX_SPEED_FLYWHEEL);
+			Acutators.getRightArmWheel().set(Constants.MIN_SPEED_FLYWHEEL);
+		}
+		
+		if (Gamepad.secondary.getLT()) {
+			Actuators.getLeftArmWheel().set(Constants.MIN_SPEED_FLYWHEEL);
+			Actuators.getRightArmWheel().set(Constants.MAX_SPEED_FLYWHEEL);
+		}
+	}
 }
